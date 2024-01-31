8f9394c8b255663e899ab7b508ec1ea2887aaa0d
chpxu <hi.progracientist@outlook.com>
2024-01-31 20:32:00 +0000

    Q2: add only even numbers and README

--- src/q2/q2.c
+++ src/q2/q2.c
@@ -24,13 +24,23 @@ int main(void)
 {
   // Upper bound is 4,000,000 in this question
   long int i;
+  double sum_of_even = 0;
   double upper_bound = 4000000;
   // Function to generate Fibonacci Numbers derived from quadratic x^2 -x - 1 = 0
   // Can solve for right size to allocate array
-  double *sequence_store = (double *)malloc(1000 * sizeof(double));
+  double numsize = 10000;
+  double *sequence_store = (double *)malloc(numsize * sizeof(double));
   size_t size = sizeof(&sequence_store) / sizeof(sequence_store[0]);
   generate_fibonacci(upper_bound, sequence_store, size);
   printf("%lf", sequence_store[size - 1]);
+  for (i = 0; i < size; i++)
+  {
+    if (!(fmod(sequence_store[i], 2) == 0))
+    {
+      continue;
+    }
+    sum_of_even += sequence_store[i];
+  }
   free(sequence_store);
   return 0;
 }
